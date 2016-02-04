f2835d91337970516fbcb09a74ad605df553b93f
c0663715 <c0663715@N208-F15-006.lambton.on.ca>
2016-02-04 02:17:38 -0500

    updated

--- src/main/java/jbosswildfly/Jbosswildfly.java
+++ src/main/java/jbosswildfly/Jbosswildfly.java
@@ -19,7 +19,7 @@ import javax.json.JsonObject;
 
 public class Jbosswildfly {
     
-    private Connection getConnection() throws SQLException {
+    private static Connection getConnection() throws SQLException {
         try {
             Class.forName("com.mysql.jdbc.Driver");
         }
@@ -37,53 +37,53 @@ public class Jbosswildfly {
         Connection con = DriverManager.getConnection(url, username, password);
         return con;
    }
-    
-    public static String getJSON() {
-        String output = "";
-        try {
-            JsonArray arr = new JsonArray();
-            Connection conn = getConnection();
-            Statement stmt = conn.createStatement();
-            ResultSet rs = stmt.executeQuery("SELECT * FROM people");
-            while (rs.next()) {
-                JsonObject obj = new JsonObject();
-              //  obj.addProperty("id", rs.getInt("id"));
-                //obj.addProperty("name", rs.getString("name"));
-                //obj.addProperty("bio", rs.getString("bio"));
-                arr.add(obj);
-            }
-            output = arr.toString();
-            conn.close();
-        } catch (SQLException ex) {
-            output = "SQL Exception Error: " + ex.getMessage();
-        }
-        return output;
-    }
-    
-    public static String getJSON(int id) {
-        String output = "";
-        try {
-            JsonArray arr = new JsonArray();
-            Connection conn = getConnection();
-            String query = "SELECT * FROM people WHERE id = 1";
-            PreparedStatement pstmt = conn.prepareStatement(query);
-            pstmt.setInt(1, id);
-            ResultSet rs = pstmt.executeQuery();
-            while (rs.next()) {
-                JsonObject obj = new JsonObject();
-                //obj.addProperty("id", rs.getInt("id"));
-                //obj.addProperty("name", rs.getString("name"));
-                //obj.addProperty("bio", rs.getString("bio"));
-                arr.add(obj);
-            }
-            output = arr.toString();
-            conn.close();
-        } catch (SQLException ex) {
-            output = "SQL Exception Error: " + ex.getMessage();
-        }
-        return output;
-    }
-
+//    
+//    public static String getJSON() {
+//        String output = "";
+//        try {
+//            JsonArray arr = new JsonArray();
+//            Connection conn = getConnection();
+//            Statement stmt = conn.createStatement();
+//            ResultSet rs = stmt.executeQuery("SELECT * FROM people");
+//            while (rs.next()) {
+//                JsonObject obj = new JsonObject();
+//              //  obj.addProperty("id", rs.getInt("id"));
+//                //obj.addProperty("name", rs.getString("name"));
+//                //obj.addProperty("bio", rs.getString("bio"));
+//                arr.add(obj);
+//            }
+//            output = arr.toString();
+//            conn.close();
+//        } catch (SQLException ex) {
+//            output = "SQL Exception Error: " + ex.getMessage();
+//        }
+//        return output;
+//    }
+//    
+//    public static String getJSON(int id) {
+//        String output = "";
+//        try {
+//            JsonArray arr = new JsonArray();
+//            Connection conn = getConnection();
+//            String query = "SELECT * FROM people WHERE id = 1";
+//            PreparedStatement pstmt = conn.prepareStatement(query);
+//            pstmt.setInt(1, id);
+//            ResultSet rs = pstmt.executeQuery();
+//            while (rs.next()) {
+//                JsonObject obj = new JsonObject();
+//                //obj.addProperty("id", rs.getInt("id"));
+//                //obj.addProperty("name", rs.getString("name"));
+//                //obj.addProperty("bio", rs.getString("bio"));
+//                arr.add(obj);
+//            }
+//            output = arr.toString();
+//            conn.close();
+//        } catch (SQLException ex) {
+//            output = "SQL Exception Error: " + ex.getMessage();
+//        }
+//        return output;
+//    }
+//
 
 
     
